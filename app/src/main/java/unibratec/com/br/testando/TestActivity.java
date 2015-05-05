59a7b7f9b5c5aa3c2080ae84d399f8da95cea264
hanndall <diguinho87@hotmail.com>
2015-05-05 00:08:29 -0300

    A

--- app/src/main/java/unibratec/com/br/testando/TestActivity.java
+++ app/src/main/java/unibratec/com/br/testando/TestActivity.java
@@ -12,7 +12,7 @@ public class TestActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
-        // TE FODE PORRRA!!!!
+        // TE FODE PORRRAA!!!!
     }
 
 
